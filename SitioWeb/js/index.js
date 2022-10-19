20e4852aca5e097dbe1f06edc166d2a888e9fcd8
Jonatan <jsantanamay@gmail.com>
2022-10-19 00:37:13 -0500

    Alternativa para líneas blancas
    Es una alternativa para solucionar las líneas que salen a los laterales de los contenedores. También se hizo algunas modificaciones al código js.

--- SitioWeb/js/index.js
+++ SitioWeb/js/index.js
@@ -1,8 +1,11 @@
 //https://developer.mozilla.org/es/docs/Web/API/EventTarget/addEventListener
 // https://www.w3schools.com/jsref/prop_element_classlist.asp
-const botonBarra = document.querySelector(".botonBarra");
-const navMenu = document.querySelector(".listaMenu");
 
-botonBarra.addEventListener("click",function(){
-    navMenu.classList.toggle("menuDesplegable")
-})
+window.onload = function(){
+    const botonBarra = document.querySelector(".botonBarra");
+    const navMenu = document.querySelector(".listaMenu");
+
+    botonBarra.addEventListener("click",function(){
+        navMenu.classList.toggle("menuDesplegable")
+    })
+}
