54b7c93fe5c449a485dbf6701317536b2368dd35
Natali <a17001060@gmail.com>
2022-11-02 17:45:42 -0600

    Ajuste
    Se eliminaron archivos para evitar duplicación en el merge

--- SitioWeb/js/index.js
+++
@@ -1,8 +0,0 @@
-//https://developer.mozilla.org/es/docs/Web/API/EventTarget/addEventListener
-// https://www.w3schools.com/jsref/prop_element_classlist.asp
-const botonBarra = document.querySelector(".botonBarra");
-const navMenu = document.querySelector(".listaMenu");
-
-botonBarra.addEventListener("click",function(){
-    navMenu.classList.toggle("menuDesplegable")
-})
