8032536dc0b31469bf68ac279912818c705384d1
Natali <a17001060@gmail.com>
2022-11-02 17:34:01 -0600

    Cambio de modos
    Implementación funcional de cambio entre modo oscuro y claro en la interfaz de pastor

--- SitioWeb/js/EstiloInterfaz.js
+++ SitioWeb/js/EstiloInterfaz.js
@@ -1,21 +1,19 @@
 function asignarModo() {
-    var estilo;
     var cadena;
+    var estilo = getCookie("estilo");
 
-    estilo = GetCookie("estilo",document.cookie);
-
-    if ( estilo == "" ) {
-        cadena = "<link rel=\"stylesheet\" href=\"interfazSesionesClaro.css\" type=\"text/css\">"
+    if ( estilo == "") {
+        cadena = "<link rel='stylesheet' href='css/interfazSesionesClaro.css'>"
         document.getElementById("bModo").value="Oscuro"
     } else {
-        cadena = "<link rel=\"stylesheet\" href=\"interfazSesiones" + estilo + "\" type=\"text/css\">"
+        cadena = "<link rel='stylesheet' href='css/interfazSesiones"+estilo+".css'>"
         if(estilo=="Oscuro"){
             document.getElementById("bModo").value="Claro";
         }else{
-            document.getElementById("bModo").value="Oscuro"
+            document.getElementById("bModo").value="Oscuro";
         }
     }
-    document.writeln (cadena);
+    document.head.innerHTML+=cadena;
 }
 
 function cambiarModo(estilo){
@@ -34,21 +32,26 @@ function SetCookie(name, value, expires) {
 }
 
 function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(";");
-    for( var i=0; i<ca.length; i++ ) {
-        var c = ca[i];
-        while( c.charAt(0)=="")
-        c = c.substring(1,c.length);
-        if( c.indexOf(nameEQ) == 0)
-            return c.substring(nameEQ.length, c.length);
+    if(document.cookie){
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(";");
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while(c.charAt(0)=="")
+                c = c.substring(1,c.length);
+            if( c.indexOf(nameEQ) == 0)
+                return c.substring(nameEQ.length, c.length);
+        }
+        return "";
+    }else{
+        return "";
     }
-    return null;
+
 }
 
-window.onload=function () {
+window.onload=function(){
     asignarModo();
-    modo=document.getElementById("bModo").getAttribute("value");
+    modo=document.getElementById("bModo").value;
     document.getElementById("bModo").onclick=function () {
         cambiarModo(modo);
     }
