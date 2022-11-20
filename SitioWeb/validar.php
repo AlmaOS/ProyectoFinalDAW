f5de2b5cbcaddacab152f6df3cdfe35a56a01d20
Jonatan <jsantanamay@gmail.com>
2022-11-20 02:00:14 -0600

    Mandando a la página dependiendo Rol
    Si eres pastor te manda a la de pastor.html.
    Si eres integrante de cualquier ministerio de manda a la de integrante.

---
+++ SitioWeb/validar.php
@@ -0,0 +1,40 @@
+<?php
+    if (empty($_REQUEST["usuario"]) || empty($_REQUEST["contrasena"])) {
+        header("location: index.php");
+        exit();
+    }
+
+    include("variables.php");
+
+    $conexion = mysqli_connect($servidor, $usuario, $contrasena, $basedatos);
+    if (!$conexion) {
+        die("Fallo: " . mysqli_connect_error());
+    }
+
+    $sql = "SELECT 	NombreUsu, contrasena, Rol, Ministerio FROM usuario WHERE NombreUsu ='" . $_REQUEST["usuario"] . "' AND contrasena ='" . $_REQUEST["contrasena"] . "'";
+    $resultado = mysqli_query($conexion, $sql);
+    mysqli_close($conexion);
+
+    for ($registros = array (); $fila = mysqli_fetch_assoc($resultado); $registros[] = $fila);
+    $numerosEntidades = count($registros);
+
+    if ($numerosEntidades > 0) {
+        $auxRol = strtoupper(strval($registros[0]["Rol"]));
+        if($auxRol == "P"){
+            header("location: pastor.html?resultado=111");
+            exit();
+        }
+        if($auxRol == "I"){
+            header("location: integrante.html?resultado=2222");
+            exit();
+        }else{
+            $auxRol = strtoupper(strval($registros[0]["Ministerio"]));
+            ($auxRol == "V")?header("location: lider.html?resultado=333"):header("location: integrante.html?resultado=444");
+            exit();
+        }
+        
+    } else {
+        header("location: index.php?resultado=2");
+    }
+
+?>
