d70d277f59e40eb7cc4d900a1717e7fecf4c71f0
Jonatan <jsantanamay@gmail.com>
2022-11-26 18:14:56 -0600

    Agregando ajax

--- SitioWeb/js/EstiloInterfaz.js
+++ SitioWeb/js/EstiloInterfaz.js
@@ -56,9 +56,9 @@ function verficiarFechaInicio(){
     if(fechaAuxiliar == ""){
         fecha = new Date();
         SetCookie("INICIO",fecha.toLocaleDateString());
-        //Ajax hace lo suyo 
-        //Se guarda en localStorable el resultado del versiculo obtenido en ajax
+        cargarContenidoAjax();
         insertarVersiculo();
+        console.log("Se inicio por primera vez");
     }else{
         fechaIngresoNuevo = new Date();
         var nuervaFechaIngre = fechaIngresoNuevo.toLocaleDateString();
@@ -66,10 +66,11 @@ function verficiarFechaInicio(){
             fecha = new Date();
             SetCookie("INICIO","");
             SetCookie("INICIO",nuervaFechaIngre);
-            //Ajax hace lo suyo 
-            //Se guarda en localStorable el resultado del versiculo obtenido en ajax
+            cargarContenidoAjax();
             insertarVersiculo();
+            console.log("Se inicio en diferente dia");
         }else{
+            console.log("Se inicio el mismo dia");
             insertarVersiculo();
         }
     }
@@ -77,19 +78,34 @@ function verficiarFechaInicio(){
 
 function insertarVersiculo(){
     //Lee del localStorable
-    texttoEjemplo = '{"booook":{"abbrev":{"pt":"sl","en":"ps"},"name":"Salmos","author":"David, Moisés, Salomão","group":"Poéticos","version":"rvr"},"chapter":117,"number":1,"text":"ALABAD á Jehová, naciones todas; Pueblos todos, alabadle."}';
+    //texttoEjemplo = '{"booook":{"abbrev":{"pt":"sl","en":"ps"},"name":"Salmos","author":"David, Moisés, Salomão","group":"Poéticos","version":"rvr"},"chapter":117,"number":1,"text":"ALABAD á Jehová, naciones todas; Pueblos todos, alabadle."}';
+    texttoEjemplo = localStorage.getItem("Versi");
     const obj = JSON.parse(texttoEjemplo);
-    localStorage.setItem("titulo", texttoEjemplo);
     var elemento = document.getElementById("prueba");
-    elemento.innerHTML = obj.text+"<br>"+"Salmos"+obj.chapter+":"+obj.number;
+    elemento.innerHTML = obj.text+"<br>"+"Proverbios "+obj.chapter+":"+obj.number;
+}
+
+function cargarContenidoAjax(){
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+        localStorage.setItem("Versi", xhttp.responseText);
+    }else{
+        localStorage.setItem("Versi", '{"book": { "abbrev": {"pt": "pv","en": "prv"}, "name": "Provérbios", "author": "Salomão","group": "Poéticos","version": "rvr"},"chapter": 27,"number": 6, "text": "Son más confiables las heridas del que ama, que los falsos besos del que aborrece."}');
+        console.log("Se llegó al limite de peticiones");
+    }
+    };
+
+    xhttp.open("GET", "https://www.abibliadigital.com.br/api/verses/rvr/prv/random", true);
+    xhttp.send();
 }
 
 window.onload=function(){
     asignarModo();
     modo = document.getElementById("bModo").value;
+    verficiarFechaInicio();
     document.getElementById("bModo").onclick=function () {
         cambiarModo(modo);
     }
-
-    verficiarFechaInicio();
 }
