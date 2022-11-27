b101cb383ad1e383c2f3ea3419a04b813b01d0d0
Jonatan <jsantanamay@gmail.com>
2022-11-27 13:44:48 -0600

    Agregando respaldo de datos formulario inventario

--- SitioWeb/js/valInventario.js
+++ SitioWeb/js/valInventario.js
@@ -1,13 +1,37 @@
-function validarInven(form){
-    if(form.fecha.value==""){
-        alert("No seleccionó la fecha");
-        form.fecha.focus();
-        return false;
+valoresGuardados = [];
+
+function guardarDatos(){
+    x = document.getElementsByClassName("check");
+    console.log(x);
+    //console.log(JSON.stringify(x));
+    for(i=0; i<x.length; i++){
+        //console.log(x[i].value);
+        console.log(x[i].checked);
     }
+    x[1].checked = true; 
 }
 
+function guardarFehca(){
+    //https://developer.mozilla.org/es/docs/Web/HTML/Element/input/date
+    //x = document.getElementsByName("fecha");
+    x = document.querySelector('input[type="date"]');
+    //console.log(x[0].value);
+    //x[0].value = "2017-06-01";
+    x.value = "2030-06-01";
+    //y = new Date(2014, 11, 11);
+    //x = y;
+}
+
+// https://www.w3schools.com/JSREF/prop_checkbox_checked.asp
 window.onload=function(){
     document.getElementById("guardar").onclick=function(){
-        validarInven(Finven);
+        if(Finven.fecha.value==""){
+            alert("No seleccionó la fecha");
+            Finven.fecha.focus();
+            return false;
+        }
+        guardarDatos();
+        guardarFehca()
+        return false;
     }
 }
