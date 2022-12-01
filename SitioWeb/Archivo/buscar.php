d197f67e84e7e4ee9644a29eae0064d6f72e1c1c
Jonatan <jsantanamay@gmail.com>
2022-12-01 01:38:22 -0600

    Descargando archivos, falta subiendo archivos

--- SitioWeb/Archivo/buscar.php
+++ SitioWeb/Archivo/buscar.php
@@ -1,10 +1,33 @@
 <?php
+    function descargarArchivo($nombreArchivo){
+        $fileimage = "../upload/".$nombreArchivo;
+        header('Content-Description: File Transfer'); 
+
+        header('Content-Type: application/octet-stream'); 
+
+        header('Content-Disposition: attachment; filename='.basename($fileimage));
+
+        header('Content-Transfer-Encoding: binary');
+
+        header('Expires: 0');
+
+        header('Cache-Control: must-revalidate'); 
+
+        header('Pragma: public');
+
+        header('Content-Length: ' . filesize($fileimage));
+
+        ob_clean(); 
+        flush(); 
+        readfile($fileimage); 
+        exit();
+    }
     /*
         Cuando el usuario quiere generar un archivo, se va ala base
         se revisa si hay ruta, si no hay, se genera y se descarga el archivo
         por otro lado si lo hay solo lo busca y descarga
-    */ 
-$fileimage = "../upload/Reporte(3).xls";
+    
+   $fileimage = "../upload/Reporte(3).xls";
    header('Content-Description: File Transfer'); //
 
    header('Content-Type: application/octet-stream'); //tipomime, vas a tener que bajar el archivo
@@ -25,4 +48,5 @@ $fileimage = "../upload/Reporte(3).xls";
    flush(); //descargo
    readfile($fileimage); //envio el archivo
    exit();
+   */
 ?>
--- SitioWeb/Archivo/descargar.php
+++ SitioWeb/Archivo/descargar.php
@@ -1,18 +1,64 @@
 <?php
-    include("../variables.php");
-    
-    $conexion = mysqli_connect($servidor, $usuario, $contrasena, $basedatos);
-	if (!$conexion) {
-    	die("Fallo: " . mysqli_connect_error());
-	}
-
-    if (!($res = $conexion->query("CALL EncontrarServicio('2022-11-30')"))) {
-        echo "Falló la llamada: (" . $mysqli->error . ") " . $mysqli->error;
+    if(empty($_REQUEST["fecha"])){
+        header("location: ../archivo.html?error=1");
+        exit();
     }
-	for ($registros = array (); $fila = mysqli_fetch_assoc($res); $registros[] = $fila);	
-    mysqli_close($conexion);
-    generarXLS($registros);
-    
+
+    $fechaAux = filter_var($_REQUEST["fecha"], FILTER_SANITIZE_STRING);
+
+    if(!validarFecha($fechaAux)){
+        header("location: ../archivo.html?error=2");
+        exit();
+    }
+
+    //Se obtienen el nombre de los archivos que están en la ruta 
+    $directorio = '../upload/';
+    $ficheros1  = scandir($directorio);
+    sort($ficheros1);
+
+    // Se busca el archvio $fechaAux
+    $fechaAux2 = $fechaAux.".xls";
+    $clave = array_search("$fechaAux", $ficheros1);
+
+    if(!$clave){
+        //No hay archivos subidos
+        generarArchivo($fechaAux);
+    }else{
+        //Sí hay archivos subidos
+        include("buscar.php");
+        descargarArchivo($fechaAux);
+    }
+
+    function validarFecha($fecha){
+        //Año - mes - dia
+        //parametros de checkDate
+        // mes, dia y año
+        $valores = explode('-', $fecha);
+        if(count($valores) == 3 && checkdate($valores[1], $valores[2], $valores[0])){
+            return true;
+        }
+        return false;
+    }
+
+    function generarArchivo($fechaServicio){
+        echo "La fecha de entrada es $fechaServicio";
+        include("../variables.php");
+
+        $conexion = mysqli_connect($servidor, $usuario, $contrasena, $basedatos);
+	    if (!$conexion) {
+    	    die("Fallo: " . mysqli_connect_error());
+	    }
+
+        if (!($res = $conexion->query("CALL EncontrarServicio('$fechaServicio')"))) {
+            mysqli_close($conexion);
+            echo "Falló la llamada: (" . $mysqli->error . ") " . $mysqli->error;
+        }
+
+        for ($registros = array (); $fila = mysqli_fetch_assoc($res); $registros[] = $fila);	
+        mysqli_close($conexion);
+        
+        generarXLS($registros);
+    }
 
     function generarXLS($array) {
         $tabla='<html><body>';
@@ -37,5 +83,20 @@
         print $tabla;
     
     }
+    /*
+    $conexion = mysqli_connect($servidor, $usuario, $contrasena, $basedatos);
+	if (!$conexion) {
+    	die("Fallo: " . mysqli_connect_error());
+	}
 
+    if (!($res = $conexion->query("CALL EncontrarServicio('2022-11-30')"))) {
+        echo "Falló la llamada: (" . $mysqli->error . ") " . $mysqli->error;
+    }
+	for ($registros = array (); $fila = mysqli_fetch_assoc($res); $registros[] = $fila);	
+    mysqli_close($conexion);
+    echo $registros[0]['Fecha'];
+    
+    generarXLS($registros);
+    
+    */
 ?>
