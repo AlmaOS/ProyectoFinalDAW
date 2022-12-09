c0cc5e9780da9a7ffa526318957894aec2880ed2
Natali <a17001060@gmail.com>
2022-12-09 09:12:27 -0600

    Correccion form modificacion

--- SitioWeb/js/valModificacionServicio.js
+++ SitioWeb/js/valModificacionServicio.js
@@ -1,39 +1,36 @@
 window.onload = function(){
-    var detener = true;
-
     document.getElementById("guardar").onclick=function(){
         return validarModificacion(Frecauds);
     }
+}
 
-    function validarModificacion(Frecauds){
-        x = document.getElementById("fechaSelec");
-        opcion =  x.options[x.selectedIndex].text;
-        if(opcion == ""){
-            alert("No seleccionó una fecha");
-            form.fechaServs.focus();
+function validarModificacion(Frecauds){
+    x = document.getElementById("fechaSelec");
+    opcion =  x.options[x.selectedIndex].text;
+    if(opcion == ""){
+        alert("No seleccionó una fecha");
+        form.fechaServs.focus();
+        return false;
+    }
+    alert("aaaa");
+
+    if(Frecauds.ofrendas.value==""){
+        return confirm("Las ofrendas no se cambiarán, ¿es correcto?");
+    }else{
+        if(isNaN(Frecauds.ofrendas.value)) {
+            alert("Cantidad de ofrendas inválida");
+            Frecauds.ofrendas.select();
             return false;
         }
+    }
 
-        if(Frecauds.ofrendas.value==""){
-            return confirm("Las ofrendas no se cambiarán, ¿es correcto?");
-        }else{
-            if(isNaN(Frecauds.ofrendas.value)) {
-                alert("Cantidad de ofrendas inválida");
-                Frecauds.ofrendas.select();
-                return false;
-            }
+    if(Frecauds.diezmo.value==""){
+        return confirm("El diezmo no se cambiará, ¿es correcto?");
+    }else {
+        if (isNaN(Frecauds.diezmo.value)) {
+            alert("Cantidad de diezmo inválida");
+            Frecauds.diezmo.select();
+            return false;
         }
-
-        if(Frecauds.diezmo.value==""){
-           return confirm("El diezmo no se cambiará, ¿es correcto?");
-        }else {
-            if (isNaN(Frecauds.diezmo.value)) {
-                alert("Cantidad de diezmo inválida");
-                Frecauds.diezmo.select();
-                return false;
-            }
-        }
-
-        return true;
     }
 }
--- SitioWeb/variables.php
+++ SitioWeb/variables.php
@@ -2,5 +2,5 @@
     $servidor = "localhost:3306";
     $usuario = "root";
     $contrasena = "";
-    $basedatos = "proyectoweb3";
+    $basedatos = "proyectowebact";
 ?>
